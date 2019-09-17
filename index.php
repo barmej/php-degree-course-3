3fc20cce990c0399b128fc4f0f1fa951fa3a2ee5
Menawer <careless.kw@gmail.com>
2019-09-18 01:26:28 +0300

    C3 L1 V5 R2

--- index.php
+++ index.php
@@ -7,8 +7,31 @@
     <title>Document</title>
 </head>
 <body>
+
+    <table border="1">
+        <tr>
+            <th>المجموعه الأولى</th>
+            <th>المجموعه الثانيه</th>
+        </tr>
+        <tr>
+            <td>فهيم</td>
+            <td>رياضي</td>
+        </tr>
+        <tr>
+            <td>مبرمج</td>
+            <td>
+                <img src="assets/images/chemist.png" width="100px" />
+                <ul>
+                    <li>كميائي</li>
+                    <li>متخصص في علوم الكيمياء</li>
+                </ul>
+            </td>
+        </tr>
+    </table>
+
     
-    <h2>الروبوتات المعتمدة في موقعنا</h2>
+    
+    <!-- <h2>الروبوتات المعتمدة في موقعنا</h2>
 
     <a target="_blank" href="assets/images/faheem.png">
         <img src="assets/images/faheem.png" />
@@ -28,6 +51,6 @@
     <a target="_blank" href="assets/images/chemist.png">
         <img src="assets/images/chemist.png" />
         <p><strong>الروبوت الكيميائي</strong></p>
-    </a>
+    </a> -->
 </body>
 </html>
