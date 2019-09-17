c7b2b3f332f6186e33971976ba9c2bd0213092f4
Menawer <careless.kw@gmail.com>
2019-09-18 02:03:47 +0300

    C3 L2 V1 R2

--- index.php
+++ index.php
@@ -5,10 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/styles.css" >
+
+    <style>
+        
+        </style>
 </head>
 <body>
 
-    <table border="1">
+    <table>
         <tr>
             <th>المجموعه الأولى</th>
             <th>المجموعه الثانيه</th>
@@ -29,28 +35,5 @@
         </tr>
     </table>
 
-    
-    
-    <!-- <h2>الروبوتات المعتمدة في موقعنا</h2>
-
-    <a target="_blank" href="assets/images/faheem.png">
-        <img src="assets/images/faheem.png" />
-        <p><strong>الروبوت فهيم</strong></p>
-    </a>
-
-    <a target="_blank" href="assets/images/sportsman.png">
-        <img src="assets/images/sportsman.png" />
-        <p><strong>الروبوت الرياضي</strong></p>
-    </a>
-
-    <a target="_blank" href="assets/images/coder.png">
-        <img src="assets/images/coder.png" />
-        <p><strong>الروبوت المبرمج</strong></p>
-    </a>
-
-    <a target="_blank" href="assets/images/chemist.png">
-        <img src="assets/images/chemist.png" />
-        <p><strong>الروبوت الكيميائي</strong></p>
-    </a> -->
 </body>
 </html>
