6320be76c91679653762464e0a83706fe0f67214
Menawer <careless.kw@gmail.com>
2019-09-18 01:02:59 +0300

    C3 L1 V4 R3

--- index.php
+++ index.php
@@ -7,10 +7,27 @@
     <title>Document</title>
 </head>
 <body>
-    <!-- التعليق هنا يمكن ان يكون بسطر واحد
-    او سطران
-    -->
-    <h1>الروبوت</h1>
-    <p>يقوم الروبوت بالرد على اغلب الاسئله الموجهه له, قم بتجربته</p>
+    
+    <h2>الروبوتات المعتمدة في موقعنا</h2>
+
+    <a target="_blank" href="assets/images/faheem.png">
+        <img src="assets/images/faheem.png" />
+        <p><strong>الروبوت فهيم</strong></p>
+    </a>
+
+    <a target="_blank" href="assets/images/sportsman.png">
+        <img src="assets/images/sportsman.png" />
+        <p><strong>الروبوت الرياضي</strong></p>
+    </a>
+
+    <a target="_blank" href="assets/images/coder.png">
+        <img src="assets/images/coder.png" />
+        <p><strong>الروبوت المبرمج</strong></p>
+    </a>
+
+    <a target="_blank" href="assets/images/chemist.png">
+        <img src="assets/images/chemist.png" />
+        <p><strong>الروبوت الكيميائي</strong></p>
+    </a>
 </body>
 </html>
