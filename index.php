8a8a4053d2173a3cc30c94c73b7a3ea84560f5dc
Menawer <careless.kw@gmail.com>
2019-09-18 00:52:30 +0300

    C3 L1 V3 R3

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <!-- التعليق هنا يمكن ان يكون بسطر واحد
+    او سطران
+    -->
+    <h1>الروبوت</h1>
+    <p>يقوم الروبوت بالرد على اغلب الاسئله الموجهه له, قم بتجربته</p>
+</body>
+</html>
