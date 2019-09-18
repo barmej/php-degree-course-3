dbd4a28acf50030dfa353820c8fe62ce26d0f4bb
Menawer <careless.kw@gmail.com>
2019-09-18 20:53:35 +0300

    C3 L3 V5 R1

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,23 @@
-function callApi(data, url){
-    console.log(data);
-    return "سنقوم بالاتصال بالموقع التالي "+url;
+
+function availableCars(car){
+    let quantity = 0;
+
+    switch(car){
+        case "تويوتا":
+            quantity = 3;
+            break;
+
+        case "نيسان":
+            quantity = 2;
+            break;
+
+        case "مرسيدس":
+            quantity = 1;
+            break;
+
+        default:
+            quantity = 0;
+    }
+
+    return quantity;
 }
